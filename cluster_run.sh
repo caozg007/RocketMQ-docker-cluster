3730b1f763b60aba0ab61c950e5e256d0421ee73
caozg007 <czg.exe@163.com>
2019-01-11 17:06:21 +0800

    Update cluster_run.sh

--- cluster_run.sh
+++ cluster_run.sh
@@ -1,7 +1,13 @@
+
+在192.168.88.247上启动broker_a_master：
 docker run -d  --name broker_a_master  --net host --restart always -v /etc/localtime:/etc/localtime:ro -v /etc/hosts:/etc/hosts:ro -e namesrvAddr="192.168.88.247:9876;192.168.88.250:9876" -e brokerIP1="192.168.88.247" -h broker_a_master 192.168.88.249/library/broker:4.3-alpine
 
+在192.168.88.247上启动broker_b_slave：
 docker run -d  --name broker_b_slave  --net host --restart always -v /etc/localtime:/etc/localtime:ro -v /etc/hosts:/etc/hosts:ro -e namesrvAddr="192.168.88.247:9876;192.168.88.250:9876" -e brokerIP1="192.168.88.247" -h broker_b_slave 192.168.88.249/library/broker:4.3-alpine
 
+
+在192.168.88.250上启动broker_b_master：
 docker run -d  --name broker_b_master  --net host --restart always -v /etc/localtime:/etc/localtime:ro -v /etc/hosts:/etc/hosts:ro -e namesrvAddr="192.168.88.247:9876;192.168.88.250:9876" -e brokerIP1="192.168.88.250" -h broker_b_master 192.168.88.249/library/broker:4.3-alpine
 
+在192.168.88.250上启动broker_a_slave：
 docker run -d  --name broker_a_slave  --net host --restart always -v /etc/localtime:/etc/localtime:ro -v /etc/hosts:/etc/hosts:ro -e namesrvAddr="192.168.88.247:9876;192.168.88.250:9876" -e brokerIP1="192.168.88.250" -h broker_a_slave 192.168.88.249/library/broker:4.3-alpine
